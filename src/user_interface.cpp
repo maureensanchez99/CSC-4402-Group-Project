84a934d8aabf3caf6a6a7ccf3894e0053e338344
LaneDurst <ltdurst03@gmail.com>
2023-11-27 22:28:18 -0600

    Added a handful of basic menu methods

--- src/user_interface.cpp
+++ src/user_interface.cpp
@@ -80,3 +80,141 @@ void TitleCard()
     std::cout << title_card << std::endl;
     std::cout << generate_border() << std::endl;
 }
+
+//might just scrap the actual login system for a simple which mode query
+//because actual security of the program isn't really important
+bool display_login_menu()
+{
+    while (true)
+    {
+        std::cout << "1: Customer Login" << std::endl << "2: Administrator Login" << std::endl;
+        char query_response = getchar();
+        std::string login_id;
+
+        //switch for promting them to enter the appropriate ID
+        switch (query_response)
+        {
+            case '1':
+                std::cout << "Enter Customer Login ID: ";
+                getline(std::cin, login_id);
+                break;
+            case '2':
+                std::cout << "Enter Admin Login ID: ";
+                getline(std::cin, login_id);
+                break;
+            default:
+                std::cout << "Invalid Selection";
+                break;
+        }
+
+        //secondary switch to check if entered values for ID match
+        /* not yet implemented
+
+        switch(query_response)
+        {
+            case '1':
+                if (isValid(login_id, customer))
+                    return true;
+                else
+                    std::cout << "Invalid Login ID"
+            case '2':
+        }
+
+        */
+    }
+}
+
+
+void display_menu(bool menu_type = false)
+{
+    //this boolean value ^ represents whether this is administrator or customer access
+    //True is administrator and False is customer, default is false
+    std::cout << generate_border() << std::endl;
+
+    std::cout << "Standard access functions:" << std::endl;
+    std::cout << "1] View item information" << std::endl;
+    std::cout << "2] Find item information" << std::endl;
+    std::cout << "3] Add item to order" << std::endl;
+    std::cout << "4] View current order" << std::endl;
+
+    if (menu_type)
+    {
+        std::cout << std::endl;
+        std::cout << "Administrator access functions:" << std::endl;
+        std::cout << "5] View employee information" <<std::endl;
+        std::cout << "6] View all orders" <<std::endl;
+        std::cout << "7] Update employee information" <<std::endl;
+        std::cout << "8] Update item information" <<std::endl;
+        std::cout << "9] Update order information" <<std::endl;
+    }
+
+    std::cout << generate_border() << std::endl;
+
+    while (true)
+    {
+        std::cout << "> ";
+        char selection = getchar();
+        switch (selection)
+        {
+            //standard stuff//
+            case '1':
+                //do stuff
+                break;
+            case '2':
+                //do stuff
+                break;
+            case '3':
+                //do stuff
+                break;
+            case '4':
+                //do stuff
+                break;
+            
+            //Admin stuff//
+            case '5':
+                if (menu_type)
+                {
+                    //do appropriate actions
+                }
+                else
+                    std::cout << "Invalid Selection:";
+                break;
+            case '6':
+                if (menu_type)
+                {
+                    //do appropriate actions
+                }
+                else
+                    std::cout << "Invalid Selection:";
+                break;
+            case '7':
+                if (menu_type)
+                {
+                    //do appropriate actions
+                }
+                else
+                    std::cout << "Invalid Selection:";
+                break;
+            case '8':
+                if (menu_type)
+                {
+                    //do appropriate actions
+                }
+                else
+                    std::cout << "Invalid Selection:";
+                break;
+            case '9':
+                if (menu_type)
+                {
+                    //do appropriate actions
+                }
+                else
+                    std::cout << "Invalid Selection:";
+                break;
+            default:
+                std::cout << "Invalid Selection:";
+                break;
+        }
+    }
+}
+
