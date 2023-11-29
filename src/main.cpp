433a847ec2a61384ce1d959160feb6af88efa3c1
Connor Morris <cmorr78@lsu.edu>
2023-11-29 13:53:04 -0600

    Part of removing the old library

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,9 @@
 #include <chrono>
 #include <thread>
 
-// Importing the official sqlite3 library
-// Install: On Linux, run this command: sudo apt-get install libsqlite3-dev
-// Source: https://www.sqlite.org/cintro.html
-#include "sqlite3.h"
-
-// Importing an unofficial sqlite3 wrapper for modern C++
-// No need to install, it's already included in external/sqlite_modern_cpp
-// Source: https://github.com/SqliteModernCpp/sqlite_modern_cpp
-#include "sqlite_modern_cpp.h"
+// Importing an unofficial sqlite3 library wrapper for C++
+// Source: https://github.com/SRombauts/SQLiteCpp/tree/sqlitecpp-release-3.3.1
+#include <SQLiteCpp/SQLiteCpp.h>
 
 #include "dblink.hpp"
 #include "sqlfunctions.hpp"
@@ -24,6 +18,16 @@ int main()
     std::pair<int, std::string> user_info;
 
     title_card();
+
+    /*
+    try {
+        SQLite::Database db("database.db");
+    }
+    catch (const std::exception& e) {
+        std::cout << "SQLite exception: " << e.what() << std::endl;
+        return EXIT_FAILURE; // unexpected error
+    }
+    */
 
     user_info = display_login_menu();
 
