f1ee5dc072ae63b0354a3514bd5e981d1d90bd58
LaneDurst <ltdurst03@gmail.com>
2023-11-29 12:01:28 -0600

    added menu function framework

--- src/checksql.hpp
+++ src/checksql.hpp
@@ -28,4 +28,124 @@ std::string name_lookup(std::string id)
     return id; // Set value manually for testing
 }
 
+
+//menu functions//
+
+
+/// @brief TODO: Add overview text for this function here
+/// @param id TODO: Add param description here
+/// @return TODO: Add return description here
+std::string view_availabe_item()
+{
+
+}
+
+/// @brief TODO: Add overview text for this function here
+/// @param id TODO: Add param description here
+/// @return TODO: Add return description here
+std::string find_an_item(int item_id)
+{
+
+}
+
+/// @brief TODO: Add overview text for this function here
+/// @param id TODO: Add param description here
+/// @return TODO: Add return description here
+std::string add_item_to_current_order(int item_id)
+{
+
+}
+
+/// @brief TODO: Add overview text for this function here
+/// @param id TODO: Add param description here
+/// @return TODO: Add return description here
+std::string remove_item_from_current_order(int item_id)
+{
+
+}
+
+/// @brief TODO: Add overview text for this function here
+/// @param id TODO: Add param description here
+/// @return TODO: Add return description here
+std::string view_current_order()
+{
+
+}
+
+/// @brief TODO: Add overview text for this function here
+/// @param id TODO: Add param description here
+/// @return TODO: Add return description here
+std::string cancel_current_order()
+{
+
+}
+
+/// @brief TODO: Add overview text for this function here
+/// @param id TODO: Add param description here
+/// @return TODO: Add return description here
+std::string checkout_current_order()
+{
+
+}
+
+
+//admin functions//
+
+
+/// @brief TODO: Add overview text for this function here
+/// @param id TODO: Add param description here
+/// @return TODO: Add return description here
+std::string view_employee_info()
+{
+
+}
+
+/// @brief TODO: Add overview text for this function here
+/// @param id TODO: Add param description here
+/// @return TODO: Add return description here
+std::string view_all_orders()
+{
+
+}
+
+/// @brief TODO: Add overview text for this function here
+/// @param id TODO: Add param description here
+/// @return TODO: Add return description here
+std::string update_employee_info(int employee_id, int pay_mod, int hours_mod, std::string job_title)
+{
+
+}
+
+/// @brief TODO: Add overview text for this function here
+/// @param id TODO: Add param description here
+/// @return TODO: Add return description here
+std::string add_employee (int employee_id, int pay, int hours, std::string job_title)
+{
+
+}
+
+/// @brief TODO: Add overview text for this function here
+/// @param id TODO: Add param description here
+/// @return TODO: Add return description here
+std::string remove_employee(int employee_id)
+{
+
+}
+
+/// @brief TODO: Add overview text for this function here
+/// @param id TODO: Add param description here
+/// @return TODO: Add return description here
+std::string update_item_info()
+{
+
+}
+
+/// @brief TODO: Add overview text for this function here
+/// @param id TODO: Add param description here
+/// @return TODO: Add return description here
+std::string update_order_info(int order_id)
+{
+
+}
+
 #endif
