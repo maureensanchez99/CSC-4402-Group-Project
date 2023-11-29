da406bf63d1a0aa700cc3e8731e6083152669788
Connor Morris <cmorr78@lsu.edu>
2023-11-29 16:29:11 -0600

    Added database file check

--- src/dblink.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef DBLINK_HPP
-#define DBLINK_HPP
-
-void connect_database()
-{
-
-}
-
-#endif
--- src/main.cpp
+++ src/main.cpp
@@ -10,27 +10,35 @@
 // Source: https://github.com/SRombauts/SQLiteCpp/tree/sqlitecpp-release-3.3.1
 #include <SQLiteCpp/SQLiteCpp.h>
 
-#include "dblink.hpp"
 #include "sqlfunctions.hpp"
 #include "userinterface.hpp"
 
+/// @brief TODO: Add overview text for this function here
+/// @return TODO: Add return description here
+std::string get_db_filepath()
+{
+    std::string current_file = std::source_location::current().file_name();
+    return current_file.substr(0, current_file.length() - std::string("src/main.cpp").length()) + "database/plant_store.db";
+}
+
 int main()
 {
-    std::pair<int, std::string> user_info;
+    const std::string db_path = get_db_filepath();
+
+    // std::cout << db_path << std::endl; // debug
+
+    try // Check database file can be accessed
+    {
+        SQLite::Database db(db_path);
+    }
+    catch (const std::exception& e) 
+    {
+        std::cout << "SQL failure: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
 
     title_card();
-
-    /*
-    try {
-        SQLite::Database db("database.db");
-    }
-    catch (const std::exception& e) {
-        std::cout << "SQLite exception: " << e.what() << std::endl;
-        return EXIT_FAILURE; // unexpected error
-    }
-    */
-
-    user_info = display_login_menu();
+    std::pair<int, std::string> user_info = display_login_menu();
 
     if (user_info.first > 0)
     {
