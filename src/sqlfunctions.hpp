69598743890064935e86b449496e85142759c0ea
LaneDurst <ltdurst03@gmail.com>
2023-11-29 21:38:09 -0600

    added two sqlfunctions

--- src/sqlfunctions.hpp
+++ src/sqlfunctions.hpp
@@ -39,7 +39,271 @@ std::pair<bool, std::string> check_id(bool employee_login, int id)
 /// @brief TODO: Add overview text for this function here
 /// @param id TODO: Add param description here
 /// @return TODO: Add return description here
-std::string view_availabe_items()
+bool view_availabe_items()
+{
+    try
+    {
+        std::cout << "All Items & Prices:" << std::endl
+        << "| Item ID   | Item Price     | Item Name      |" << std::endl //|10 char|15 char|//
+        << "-----------------------------------------------" << std::endl;
+        SQLite::Database db(get_db_filepath());
+        SQLite::Statement query1(db, "SELECT product_id, price, name FROM product NATURAL JOIN plant WHERE product.product_id = plant.product_id");
+        while(query1.executeStep())
+        {
+           std::string id = query1.getColumn(0);
+           std::string price = query1.getColumn(1);
+           std::string name = query1.getColumn(2);
+           std::string output = "| "+id;
+           while (output.size() < 12)
+           {
+            output +=" ";
+           }
+           output += "| "+price;
+           while (output.size() < 29)
+           {
+            output +=" ";
+           }
+           output += "| "+name;
+           while (output.size() < 46)
+           {
+            output +=" ";
+           }
+           output +="|";
+           std::cout << output << std::endl;
+        }
+        SQLite::Statement query2(db, "SELECT product_id, price, type FROM product NATURAL JOIN tool WHERE product.product_id = tool.product_id");
+        while(query2.executeStep())
+        {
+           std::string id = query2.getColumn(0);
+           std::string price = query2.getColumn(1);
+           std::string name = query2.getColumn(2);
+           std::string output = "| "+id;
+           while (output.size() < 12)
+           {
+            output +=" ";
+           }
+           output += "| "+price;
+           while (output.size() < 29)
+           {
+            output +=" ";
+           }
+           output += "| "+name;
+           while (output.size() < 46)
+           {
+            output +=" ";
+           }
+           output +="|";
+           std::cout << output << std::endl;
+        }
+        SQLite::Statement query3(db, "SELECT product_id, price, name FROM product NATURAL JOIN seed WHERE product.product_id = seed.product_id");
+        while(query3.executeStep())
+        {
+           std::string id = query3.getColumn(0);
+           std::string price = query3.getColumn(1);
+           std::string name = query3.getColumn(2);
+           std::string output = "| "+id;
+           while (output.size() < 12)
+           {
+            output +=" ";
+           }
+           output += "| "+price;
+           while (output.size() < 29)
+           {
+            output +=" ";
+           }
+           output += "| "+name;
+           while (output.size() < 46)
+           {
+            output +=" ";
+           }
+           output +="|";
+           std::cout << output << std::endl;
+        }
+        return true;
+    }
+    catch (const std::exception& e) {std::cout << "SQL failure: " << e.what() << std::endl;}
+    return false;
+}
+
+/// @brief TODO: Add overview text for this function here
+/// @param id TODO: Add param description here
+/// @return TODO: Add return description here
+bool find_an_item(int item_id)
+{
+    try
+    {
+        SQLite::Database db(get_db_filepath());
+        if (item_id <= 3)
+        {
+            SQLite::Statement query(db, "Select * FROM plant NATURAL JOIN product WHERE plant.product_id = " + std::to_string(item_id));
+            while (query.executeStep())
+            {
+                std::string col1 = query.getColumn(0);
+                std::string col2 = query.getColumn(1);
+                std::string col3 = query.getColumn(2);
+                std::string col4 = query.getColumn(3);
+                std::string col5 = query.getColumn(4);
+                std::string col6 = query.getColumn(5);
+                std::string col7 = query.getColumn(6);
+                std::string col8 = query.getColumn(7);
+                std::string output ="|"+col1;
+                //11 19 31 44 55 67 75
+                while (output.size() < 5)
+                {
+                    output += " ";
+                }
+                output +="|"+col2;
+                while (output.size() < 19)
+                {
+                    output += " ";
+                }
+                output +="|"+col3;
+                while (output.size() < 34)
+                {
+                    output += " ";
+                }
+                output +="|"+col4;
+                while (output.size() < 44)
+                {
+                    output += " ";
+                }
+                output +="|"+col5;
+                while (output.size() < 59)
+                {
+                    output += " ";
+                }
+                output +="|"+col6;
+                while (output.size() < 74)
+                {
+                    output += " ";
+                }
+                output +="|"+col7;
+                while (output.size() < 89)
+                {
+                    output += " ";
+                }
+                output +="|"+col8;
+                while (output.size() < 99)
+                {
+                    output += " ";
+                }
+                output += "|";
+                std::cout << output << std::endl;
+            }
+        }
+        else if (item_id <= 6)
+        {
+            std::cout << "|ID  |Type         |Brand         | Price   |" << std::endl;
+            SQLite::Statement query(db, "Select * FROM tool NATURAL JOIN product WHERE tool.product_id = " + std::to_string(item_id));
+            while (query.executeStep())
+            {
+                std::string col1 = query.getColumn(0);
+                std::string col2 = query.getColumn(1);
+                std::string col3 = query.getColumn(2);
+                std::string col4 = query.getColumn(3);
+                std::string output ="|"+col1;
+                while (output.size() < 5)
+                {
+                    output += " ";
+                }
+                output +="|"+col2;
+                while (output.size() < 19)
+                {
+                    output += " ";
+                }
+                output +="|"+col3;
+                while (output.size() < 34)
+                {
+                    output += " ";
+                }
+                output +="|"+col4;
+                while (output.size() < 44)
+                {
+                    output += " ";
+                }
+                output += "|";
+                std::cout << output << std::endl;
+            }
+        }
+        else if (item_id <= 9)
+        {
+            std::cout << "|ID  |Name      |Type       |Season   |Zone|Sunlight      |Water        |Humidity           |Price |" << std::endl
+                      << "----------------------------------------------------------------------------------------------------" << std::endl;
+            SQLite::Statement query(db, "Select * FROM seed NATURAL JOIN product WHERE seed.product_id = " + std::to_string(item_id));
+            while (query.executeStep())
+            {
+                std::string col1 = query.getColumn(0);
+                std::string col2 = query.getColumn(1);
+                std::string col3 = query.getColumn(2);
+                std::string col4 = query.getColumn(3);
+                std::string col5 = query.getColumn(4);
+                std::string col6 = query.getColumn(5);
+                std::string col7 = query.getColumn(6);
+                std::string col8 = query.getColumn(7);
+                std::string col9 = query.getColumn(8);
+                std::string output ="|"+col1;
+                //11 19 31 44 55 67 75
+                while (output.size() < 5)
+                {
+                    output += " ";
+                }
+                output +="|"+col2;
+                while (output.size() < 16)
+                {
+                    output += " ";
+                }
+                output +="|"+col3;
+                while (output.size() < 28)
+                {
+                    output += " ";
+                }
+                output +="|"+col4;
+                while (output.size() < 38)
+                {
+                    output += " ";
+                }
+                output +="|"+col5;
+                while (output.size() < 43)
+                {
+                    output += " ";
+                }
+                output +="|"+col6;
+                while (output.size() < 58)
+                {
+                    output += " ";
+                }
+                output +="|"+col7;
+                while (output.size() < 72)
+                {
+                    output += " ";
+                }
+                output +="|"+col8;
+                while (output.size() < 92)
+                {
+                    output += " ";
+                }
+                output +="|"+col9;
+                while (output.size() < 99)
+                {
+                    output += " ";
+                }
+                output += "|";
+                std::cout << output << std::endl;
+            }
+        }
+        return true;
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << "SQL failure: " << e.what() << std::endl;
+    }
+    return false;
+}
+
+/// @brief TODO: Add overview text for this function here
+/// @param id TODO: Add param description here
+/// @return TODO: Add return description here
+std::string add_item_to_current_order(int item_id)
 {
     return std::string();
 }
@@ -47,23 +311,7 @@ std::string view_availabe_items()
 /// @brief TODO: Add overview text for this function here
 /// @param id TODO: Add param description here
 /// @return TODO: Add return description here
-std::string find_an_item(std::string item_name)
-{
-    return std::string();
-}
-
-/// @brief TODO: Add overview text for this function here
-/// @param id TODO: Add param description here
-/// @return TODO: Add return description here
-std::string add_item_to_current_order(std::string item_name)
-{
-    return std::string();
-}
-
-/// @brief TODO: Add overview text for this function here
-/// @param id TODO: Add param description here
-/// @return TODO: Add return description here
-std::string remove_item_from_current_order(std::string item_name)
+std::string remove_item_from_current_order(int item_id)
 {
     return std::string();
 }
@@ -113,7 +361,7 @@ std::string view_all_orders()
 /// @brief TODO: Add overview text for this function here
 /// @param id TODO: Add param description here
 /// @return TODO: Add return description here
-std::string update_employee_info(int employee_id, int pay_mod, int hours_mod, std::string job_title)
+std::string update_employee_info(int employee_id, std::string name, std::string address, int hours_mod, int wage, bool is_manager)
 {
     return std::string();
 }
@@ -121,7 +369,7 @@ std::string update_employee_info(int employee_id, int pay_mod, int hours_mod, st
 /// @brief TODO: Add overview text for this function here
 /// @param id TODO: Add param description here
 /// @return TODO: Add return description here
-std::string add_employee (int employee_id, int pay, int hours, std::string job_title)
+std::string add_employee (int employee_id, std::string name, std::string address, int hours_mod, int wage, bool is_manager)
 {
     return std::string();
 }
